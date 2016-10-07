a5940d99b68dca528732b3dd57fbdf399e4186eb
Romel John Santos <romel@romeljohnsantos.com>
2016-10-07 13:20:37 +0800

    Version 3.1 Update

--- config/database.php
+++ config/database.php
@@ -13,8 +13,8 @@
     array (
       'driver' => 'mysql',
       'host' => 'localhost',
-      'database' => ‘shinedb’,
-      'username' => ‘root’,
+      'database' => 'shinedb',
+      'username' => 'root',
       'password' => '',
       'charset' => 'utf8',
       'collation' => 'utf8_unicode_ci',
